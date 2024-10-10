7cba9d3e3cc075d014ff8ae5e2f53ef957568540
Pinukio <59557081+Pinukio@users.noreply.github.com>
2024-10-10 22:31:48 +0900

    Title: 홀수만 출력하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 241010/홀수만 출력하기/print-only-odd-numbers.cpp
+++ 241010/홀수만 출력하기/print-only-odd-numbers.cpp
@@ -7,7 +7,7 @@ int main() {
     int input;
     for(int i = 0; i < n; i++) {
         cin >> input;
-        if(input%3 == 0)
+        if(input%2 == 1 && input%3 == 0)
             cout << input << endl;
     }
     return 0;
