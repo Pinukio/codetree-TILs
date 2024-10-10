9766abc69d6ed875dca71418003c42fce02db8c5
Pinukio <59557081+Pinukio@users.noreply.github.com>
2024-10-10 22:14:14 +0900

    Title: 4번의 크기 비교, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 241010/4번의 크기 비교/4-time-comparison.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a, b, c, d, e;
+    cin >> a >> b >> c >> d >> e;
+    if(a > b)
+        cout << 1;
+    else
+        cout << 0;
+    cout << endl;
+
+    if(a > c)
+        cout << 1;
+    else
+        cout << 0;
+    cout << endl;
+    
+    if(a > d)
+        cout << 1;
+    else
+        cout << 0;
+    cout << endl;
+
+    if(a > e)
+        cout << 1;
+    else
+        cout << 0;
+    cout << endl;
+
+    return 0;
+}
