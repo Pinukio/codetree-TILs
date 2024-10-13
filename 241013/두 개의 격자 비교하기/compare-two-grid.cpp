3774e422ad60df1b4f1417bb1492646ae60ddcc2
Pinukio <59557081+Pinukio@users.noreply.github.com>
2024-10-13 15:20:19 +0900

    Title: 두 개의 격자 비교하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 241013/두 개의 격자 비교하기/compare-two-grid.cpp
+++ 241013/두 개의 격자 비교하기/compare-two-grid.cpp
@@ -4,14 +4,14 @@ using namespace std;
 int main() {
     int n, m, i, j, input;
     cin >> n >> m;
-    int inputArr[100][100], resultArr[100][100];
+    int inputArr[10][10];
     for(i = 0; i < n; i++) {
-        for(j = 0; j < n; j++) {
+        for(j = 0; j < m; j++) {
             cin >> inputArr[i][j];
         }
     }
     for(i = 0; i < n; i++) {
-        for(j = 0; j < n; j++) {
+        for(j = 0; j < m; j++) {
             cin >> input;
             cout << !(input == inputArr[i][j]) << " ";
         }
