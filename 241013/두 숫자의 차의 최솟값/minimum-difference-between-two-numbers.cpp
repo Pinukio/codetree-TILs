d48a77cdaebb782c537e8c18690a14caf7187b8d
Pinukio <59557081+Pinukio@users.noreply.github.com>
2024-10-13 15:05:14 +0900

    Title: 두 숫자의 차의 최솟값, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 241013/두 숫자의 차의 최솟값/minimum-difference-between-two-numbers.cpp
+++ 241013/두 숫자의 차의 최솟값/minimum-difference-between-two-numbers.cpp
@@ -8,7 +8,9 @@ int main() {
     cin >> n;
     for(int i = 0; i < n; i++) {
         cin >> arr[i];
-        for(int j = i-1; j > 0; j--) {
+    }
+    for(int i = 0; i < n; i++) {
+        for(int j = i+1; j < n; j++) {
             diff = arr[i]-arr[j];
             diff = (diff > 0) ? diff : -diff;
             if(diff < minDiff)
