f4d620ca358bc3593450c5a2b0c9c1089dbe82a5
Pinukio <59557081+Pinukio@users.noreply.github.com>
2024-10-13 16:42:02 +0900

    Title: 공백을 기준으로 출력, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 241013/공백을 기준으로 출력/output-based-on-space.cpp
+++ 241013/공백을 기준으로 출력/output-based-on-space.cpp
@@ -3,7 +3,12 @@ using namespace std;
 
 int main() {
     string result, input;
+    int cnt = 0;
     while(cin.peek() != string::npos) {
+        if(cin.peek() == '\n')
+            cnt++;
+        if(cnt == 2)
+            break;
         cin >> input;
         result += input;
     }
