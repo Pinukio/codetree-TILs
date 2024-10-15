6ab3bbc5b7aae750879d2fc4ccaef9cc02e3bddd
Pinukio <59557081+Pinukio@users.noreply.github.com>
2024-10-15 21:25:15 +0900

    Title: 금 채굴하기, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 241015/금 채굴하기/gold-mining.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+using namespace std;
+
+int grid[20][20];
+
+int howManyGold(int centerX, int centerY, int n, int k) {
+    int gold = 0, cnt=-1;
+    for(int i = centerY-k; i <= centerY+k; i++) {
+        if(i <= centerY)
+            cnt++;
+        else
+            cnt--;
+        if(i < 0 || i >= n)
+            continue;
+
+        for(int j = centerX-cnt; j <= centerX+cnt; j++) {
+            if(j < 0 || j >= n)
+                continue;
+            
+            gold += grid[i][j];
+        }
+    }
+    return gold;
+}
+
+int main() {
+    int n, m, maxGold=0, cost, gold, efficiency;
+    cin >> n >> m;
+
+    for(int i = 0; i < n; i++) {
+        for(int j = 0; j < n; j++) {
+            cin >> grid[i][j];
+        }
+    }
+
+    for(int k = 0; k < n; k++) {
+        cost = k*k + (k+1)*(k+1);
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j < n; j++) {
+                gold = howManyGold(j, i, n, k);
+                efficiency = gold*m - cost;
+                if(efficiency > 0) {
+                    maxGold = gold;
+                }
+            }
+        }
+    }
+
+    cout << maxGold;
+
+    return 0;
+}
