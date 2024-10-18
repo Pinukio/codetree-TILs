bf7e4e82de2dee5ba6ccd928f5342ce4f18cf132
Pinukio <59557081+Pinukio@users.noreply.github.com>
2024-10-18 23:05:55 +0900

    Title: 겹치지 않게 선분 고르기, Time: 13ms, Memory: 0MB, Status: Passed - CodeTree

--- 241018/겹치지 않게 선분 고르기/select-segments-without-overlap.cpp
+++ 241018/겹치지 않게 선분 고르기/select-segments-without-overlap.cpp
@@ -19,7 +19,7 @@ bool isOverlapped() {
                 continue;
             x3 = lines[j].first;
             x4 = lines[j].second;
-            if((x2 >= x3 && x2 <= x4)||(x3 >= x1 && x3 <= x2)) 
+            if((x2 >= x3 && x2 <= x4)||(x3 >= x1 && x3 <= x2) || (x1 >= x3 && x1 <= x4) || (x4 >= x1 && x4 <= x2)) 
                 return true;
         }
     }
